89a71e9b19e60be4ab6eacdf47de408892e64008
attachesoftware\StevenG <steveng@attachesoftware.com>
2018-03-14 17:17:52 +1100

    Adding a Datastore and Increasing the Complexity : Adding Mongo to the App

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var cats = require('./cats.js')(app);
+var cats = require('./routes/cat_routes.js')(app);
 
 var server = cats.listen(3000, function () {
     console.log('Server running at http://127.0.0.1:3000/'); 
