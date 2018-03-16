f98f48cc6967138998f008fd52f4ec58afbf266b
attachesoftware\StevenG <steveng@attachesoftware.com>
2018-03-16 14:39:44 +1100

    Adding Async : Processing Requests Asynchronously

--- routes/pet_routes.js
+++ routes/pet_routes.js
@@ -1,29 +1,52 @@
 var r = require('request').defaults({
-    json: true
+  json: true
 });
 
-module.exports = function (app) {
-    //Read
-    app.get('/pets', function (req, res) {
-        r({ uri: 'http://localhost:3001/dog' }, function (err, response, body) {
-            if (!err && response.statusCode === 200) {
-                res.json(body);
-            } else {
-                res.send(response.statusCode);
-            }
-        });
+var async = require('async');
 
-        r({ uri: 'http://localhost:3000/cat' }, function (err, response, body) {
-            if (!err && response.statusCode === 200) {
-                res.json(body);
-            } else {
-                res.send(response.statusCode);
-            }
+module.exports = function(app) {
+  //Read
+  app.get('/pets', function(req, res) {
+    async.parallel(
+      {
+        cat: function (callback) {
+            r({ uri: 'http://localhost:3000/cat' }, function (err, response, body) {
+                if (err) {
+                    callback({ service: 'cat', error: error });
+                    return;
+                };
+                if (!err && response.statusCode == 200) {
+                    callback(null, body.data);
+                } else {
+                    callback(response.statusCode);
+                }
+            });
+        },
+        dog: function (callback) {
+            r({ uri: 'http://localhost:3001/dog' }, function (err, response, body) {
+                if (err) {
+                    callback({ service: 'dog', error: error });
+                    return;
+                };
+                if (!err && response.statusCode == 200) {
+                    callback(null, body.data);
+                } else {
+                    callback(response.statusCode);
+                }
+            });
+        }
+      },
+      function(error, results) {
+        res.json({
+          error: error,
+          results: results
         });
-    });
+      }
+    );
+  });
 
-    app.get('/ping', function (req, res) {
-        res.json({ pong: Date.now() });
-    });
-    return app;
+  app.get('/ping', function(req, res) {
+    res.json({ pong: Date.now() });
+  });
+  return app;
 };
